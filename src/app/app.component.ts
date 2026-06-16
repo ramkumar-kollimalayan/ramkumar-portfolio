a6ba86de0008e0e058a093a40b6c2226c2c2d9de
Ramkumar MariApps <ramkumar.kollimalayan@pal-erp.com>
2026-06-16 11:26:56 +0530

    Stats section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,12 @@ import { SkillsComponent } from './sections/skills/skills.component';
 import { ExperienceComponent } from './sections/experience/experience.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
 import { ContactComponent } from './sections/contact/contact.component';
+import { StatsComponent } from './sections/stats/stats.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
+  imports: [NavbarComponent, HeroComponent, StatsComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+
+interface Star {
+  left: number;
+  top: number;
+  size: number;
+  delay: number;
+  duration: number;
+}
 
 @Component({
   selector: 'app-hero',
-  imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
 
+  mouseX = 0;
+  mouseY = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+
+    const glow = document.querySelector('.cursor-glow') as HTMLElement;
+
+    if (!glow) return;
+
+    glow.style.left = `${event.clientX}px`;
+    glow.style.top = `${event.clientY}px`;
+  }
+
+  stars: Star[] = Array.from({ length: 90 }, () => ({
+    left: Math.random() * 100,
+    top: Math.random() * 100,
+    size: Math.random() * 2.2 + 0.8,
+    delay: Math.random() * 5,
+    duration: Math.random() * 3 + 3
+  }));
 }
